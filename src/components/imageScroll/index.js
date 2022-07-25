18f012779f0d0a6b887d735af4c9971911213d34
del-alj <douae.elalj@gmail.com>
2022-07-25 10:06:06 +0100

    add script for resize v 2

--- src/components/imageScroll/index.js
+++ src/components/imageScroll/index.js
@@ -1,60 +1,62 @@
 import React, { useEffect, useState } from "react";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 import { Box, Div } from "./style";
 
 export const ImageScroll = (props) => {
-    const [page, setPage] = useState(1);
-    const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
 
-    useEffect(() => {
-        const temp = props?.images
-        const index = 6 * page;
+  useEffect(() => {
+    const temp = props?.images;
+    const index = 6 * page;
 
-        setImages(
-            temp?.slice(0, (index < temp?.length ? index : temp?.length)), 
-        )
-    }, [page])
+    setImages(temp?.slice(0, index < temp?.length ? index : temp?.length));
 
+    hires_images();
+  }, [page]);
 
-const hires_images = () => {
-    var imgs = document.getElementsByTagName('img');
-    for (let i = 0; i < imgs.length; i++) { 
-        if (imgs[i].realuri) {
-            imgs[i].src=imgs[i].realuri;
-        }
-     }
-}
-if (window.addEventListener) {
-     window.addEventListener('load', hires_images, false);
-} else if (window.attachEvent) {
-    window.attachEvent('onload', hires_images);
-}
+  const hires_images = () => {
+    let imgs = document.getElementsByTagName("img");
+    for (let i = 0; i < imgs.length; i++) {
+      if (imgs[i]) {
+        let img = imgs[i];
+        let attr = img?.getAttribute("realurl");
+        imgs[i].src = attr;
+      }
+    }
+  };
+  if (window.addEventListener) {
+    window.addEventListener("load", hires_images, false);
+  } else if (window.attachEvent) {
+    window.attachEvent("onload", hires_images);
+  }
 
-    return (
-        <Box>
-            <InfiniteScroll
-                dataLength={images.length}
-                next={() => setPage(page + 1)}
-                hasMore={true} >
-                {images?.map((elem, i) => {
-                    return <Div key={i}>
-                        <p >{elem}</p>
-                        <img
-                            realurl={`https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${elem}`}
-                            src={"data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7"}
-                            alt={elem}
-                            width="300"
-                            height="400" 
-                            
-                            />
-                           {hires_images}
-                          
-                    </Div>
-                })}
-            </InfiniteScroll>
-        </Box>
-
-    )
-}
+  return (
+    <Box>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={() => setPage(page + 1)}
+        hasMore={true}
+      >
+        {images?.map((elem, i) => {
+          return (
+            <Div key={i}>
+              <p>{elem}</p>
+              <img
+                realurl={`https://neoos.s3.eu-west-1.amazonaws.com/img/birds/${elem}`}
+                src={
+                  "data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7"
+                }
+                alt={elem}
+                width="300"
+                height="400"
+              />
+            </Div>
+          );
+        })}
+      </InfiniteScroll>
+    </Box>
+  );
+};
